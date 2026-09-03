46a42cc58bfe68c873c33462bf8eeb0d9c327746
Cass Krosby <mjk111@humboldt.edu>
2026-09-02 19:33:37 -0700

    Part 1 done

---
+++ hw_1/325hw1-1.sql
@@ -0,0 +1,32 @@
+-- Cass Krosby, 2026-09-02
+-- 325 HW 1 - my first table
+
+-- Turn on spooling for the work
+spool 325hw1-out.txt;
+
+-- Table: my_table
+drop table my_table;
+create table my_table (
+    item_id:    Integer,
+    item_name:  varchar2(40),
+    item_count: Integer,
+    item_price: Float,
+    primary key (item_id)
+);
+
+-- Need numbers in the table!
+begin
+    for id in 1..5 loop
+        insert into my_table (item_id, item_count, item_price) values (id, id * 17 - 3, 1.50 / id);
+    end loop;
+end;
+/
+
+-- Give rows creative names
+update table my_table set item_name = 'Life Stories of John Lennon'            where item_id = 1;
+update table my_table set item_name = 'The Secret Lives of Swiss Cheesemakers' where item_id = 2;
+update table my_table set item_name = 'Your Aunt''s Cabinet'                   where item_id = 3;
+update table my_table set item_name = 'The Father of the Bride'                where item_id = 4;
+update table my_table set item_name = 'Silly Symphonies, vol. 5'               where item_id = 5;
+
+spool off;
