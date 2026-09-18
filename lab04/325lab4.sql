68e326426028c74d4b0dea177e35e18782125128
Cass Krosby <mjk111@humboldt.edu>
2026-09-18 15:32:44 -0700

    Fix various things with prompt

--- lab04/325lab4.sql
+++ lab04/325lab4.sql
@@ -5,15 +5,18 @@ start 'set-up-ex-tbls.sql';
 
 spool '325lab4-out.txt';
 
-prompt 'true relational selections';
+prompt Cass Krosby - Week 4 Lab;
+prompt ;
+
+prompt true relational selections;
 select * from empl where empl_num = '7900';
 select * from dept where dept_num = '100';
-prompt 'true relational projections';
+prompt true relational projections;
 select distinct cust_city, cust_state from customer;
 select distinct empl_last_name, job_title from empl;
-prompt 'true Cartesian product';
+prompt true Cartesian product;
 select * from empl, dept;
-prompt 'true equi-join of customer and empl';
+prompt true equi-join of customer and empl;
 select * from customer, empl where customer.empl_rep = empl.empl_num;
 
 spool off;
